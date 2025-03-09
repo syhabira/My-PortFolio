8ffc5e5661a4e4544be2804e8a11a3f17097b601
syhabira <aylashabira17@gmail.com>
2025-03-10 05:30:22 +0700

    hai

--- tailwind/main.js
+++ tailwind/main.js
@@ -21,22 +21,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
-gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
-gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: "expo.inOut"});
+    gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: "expo.inOut"});
+    gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: "expo.inOut"});
 
+    // IMG Animation
+    gsap.from('.home_img', {opacity: 0, duration: 2, delay: 2, x: 60});
 
-// IMG
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
+    // INFORMATION
+    gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25});
+    gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease: "expo.out", stagger: 0.3});
 
-// INFORMATION
-gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
-gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
+    // NAV ITEM
+    gsap.from('.nav__logo', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease: "expo.out"});
+    gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease: "expo.out", stagger: 0.2});
 
-// NAV ITEM
-gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
-gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
+    // SOCIAL ICONS
+    gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease: "expo.out", stagger: 0.2});
+});
+gsap.from('.nav', {opacity: 0, duration: 2, delay: 2, x: 60});
 
-// SOCIAL
-gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
-  
